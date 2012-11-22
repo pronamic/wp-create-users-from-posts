7a4e85795089bb2238da0d9d6ed2be7156fc1baf
Remco Tolsma <info@remcotolsma.nl>
2012-11-22 15:02:26 +0100

    Removed prefix spaces in plugin header.

--- create-users-from-posts.php
+++ create-users-from-posts.php
@@ -1,13 +1,13 @@
 <?php
 /*
- Plugin Name: Create Users From Posts
- Plugin URI: http://pronamic.eu/wordpress/create-users-from-posts/
- Description: Gets the properties of a post and converts it to the properties of a user
- Version: 0.1
- Requires at least: 3.0
- Author: Pronamic
- Author URI: http://pronamic.eu/
- License: GPLv2
+Plugin Name: Create Users From Posts
+Plugin URI: http://pronamic.eu/wordpress/create-users-from-posts/
+Description: Gets the properties of a post and converts it to the properties of a user
+Version: 0.1
+Requires at least: 3.0
+Author: Pronamic
+Author URI: http://pronamic.eu/
+License: GPLv2
 */
 
 /**
